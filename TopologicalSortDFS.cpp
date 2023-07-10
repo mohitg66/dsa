ed1ecb9f7f927d632759a59d207485242e3951cd
Mohit <mohit21339@iiitd.ac.in>
2023-07-10 19:01:45 +0530

    first commit

---
+++ TopologicalSortDFS.cpp
@@ -0,0 +1,26 @@
+    stack<int> s;
+    vector<int> traversal;
+    vector<bool> visited;
+
+    void dfs(vector<int> adj[], int i){
+        visited[i]= 1;
+        for (int next: adj[i]){
+            if (!visited[next])
+                dfs(adj, next);
+        }
+        s.push(i);
+    }
+
+    vector<int> topoSort(int V, vector<int> adj[])
+    {
+        visited= vector<bool>(V,false);
+        for (int i=0; i<V; i++){
+            if (!visited[i])
+                dfs(adj, i);
+        }
+        while (!s.empty()) {
+            traversal.push_back(s.top());
+            s.pop();
+        }
+        return traversal;
+    }
